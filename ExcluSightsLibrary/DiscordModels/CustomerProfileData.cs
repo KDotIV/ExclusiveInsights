81a4da20e1f996ebd4374e82a1449036a090edea
FiendPopsicle <fiendpopsicle@gmail.com>
2025-09-01 15:55:50 -0400

    - Models added

---
+++ ExcluSightsLibrary/DiscordModels/CustomerProfileData.cs
@@ -0,0 +1,11 @@
+﻿namespace ExcluSightsLibrary.DiscordModels
+{
+    public class CustomerProfileData
+    {
+        public required string CustomerId { get; set; }
+        public required ulong DiscordId { get; set; }
+        public required string DiscordTag { get; set; }
+        public string? CustomerFirstName { get; set; }
+        public string? CustomerLastName { get; set; }
+    }
+}
---
+++ ExcluSightsLibrary/DiscordModels/SolePlayDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcluSightsLibrary.DiscordModels
+{
+    public class SolePlayDTO : CustomerProfileData
+    {
+        public required double ShoeSize { get; set; }
+        public required Interests Interests { get; set; }
+        public required int Gender { get; set; }
+    }
+    public enum Interests
+    {
+        None = 0,
+        Gaming = 1,
+        Fashion = 2,
+        Other = 16
+    }
+}
